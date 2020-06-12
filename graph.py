69517126dc1da465efff3a67dc862d90b4c1510a
Tal Hayut <talhayut.dev@gmail.com>
2020-06-12 13:50:35 +0300

    feat: adding graph

--- graph.py
+++ graph.py
@@ -3,7 +3,7 @@ from queue import Queue
 
 class Graph:
     def __init__(self):
-        self.adjacency = {}
+        self.adjacency = dict()
         self.queue = Queue()
 
     def add_vertex(self, val):
@@ -20,26 +20,10 @@ class Graph:
         self.queue.enqueue(vertex)
         while self.queue.length:
             current = self.queue.dequeue()
-            for neighbor in self.adjacency[current]:
+            neighbors = self.adjacency[current]
+            for neighbor in neighbors:
                 if not neighbor in visited:
                     visited[neighbor] = True
+                    self.queue.enqueue(neighbor)
                     result.append(neighbor)
-                    self.queue.enqueue(self.adjacency[neighbor])
         print(result)
-
-
-g = Graph()
-g.add_vertex("A")
-g.add_vertex("B")
-g.add_vertex("C")
-g.add_vertex("D")
-g.add_vertex("E")
-g.add_vertex("F")
-g.add_edge("A", "B")
-g.add_edge("A", "C")
-g.add_edge("B", "D")
-g.add_edge("C", "E")
-g.add_edge("D", "E")
-g.add_edge("D", "F")
-g.add_edge("E", "F")
-g.print("A")
